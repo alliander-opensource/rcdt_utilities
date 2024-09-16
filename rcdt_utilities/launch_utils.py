bfd458d1973e74bb870fdcdda54e375e98266021
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2024-09-16 10:20:55 +0200

    Add launcher tool
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- rcdt_utilities/launch_utils.py
+++ rcdt_utilities/launch_utils.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import sys
 from typing import List
 import os
 import yaml
@@ -10,8 +11,40 @@ import xacro
 from ament_index_python.packages import get_package_share_directory
 
 
+class LaunchArguments:
+    def __init__(self) -> None:
+        self.values = {}
+        self.options = {}
+
+    def add_value(self, name: str, value: str, options: List[str]) -> None:
+        self.set_value(name, value)
+        self.options[name] = options
+
+    def set_value(self, name: str, value: str) -> None:
+        self.values[name] = value
+
+    def get_value(self, name: str) -> str:
+        return self.values[name]
+
+    def get_options(self, name: str) -> List[str]:
+        return self.options[name]
+
+    def all_values(self) -> dict:
+        return self.values
+
+    def update_from_sys(self) -> None:
+        for arg in sys.argv:
+            if ":=" in arg:
+                name, value = arg.split(":=")
+                self.set_value(name, value)
+
+
+def get_package_path(package: str) -> str:
+    return get_package_share_directory(package)
+
+
 def get_file_path(package: str, folders: List[str], file: str) -> str:
-    package_path = get_package_share_directory(package)
+    package_path = get_package_path(package)
     return os.path.join(package_path, *folders, file)
 
 
