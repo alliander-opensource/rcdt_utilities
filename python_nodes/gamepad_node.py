ce8de1922e00ae857a1cec0a93a4155c1be48d9f
Jelmer de Wolde <Jelmerdw@users.noreply.github.com>
2024-09-27 13:39:46 +0200

    Gripper support (#5)
    * Add launcher tool
    
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>
    
    * Remove catalog-info.
    
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>
    
    * Undo changes to README.
    
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>
    
    * Start with multi gamepad support and rotations.
    
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>
    
    * Add xbox button mapping.
    
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>
    
    * Refactor gamepad.
    
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>
    
    * Fix license.
    
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>
    
    * Add epsilon to always succeed grasping.
    
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>
    
    * Add flip option for axes.
    
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>
    
    * Add support for third angular axis.
    
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>
    
    * Add third angular movement to virtual gamepad.
    
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>
    
    * Fix check on gripper move.
    
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>
    
    ---------
    
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- python_nodes/gamepad_node.py
+++ python_nodes/gamepad_node.py
@@ -4,55 +4,76 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from dataclasses import dataclass
 import rclpy
 from rclpy.node import Node
+from rclpy.action import ActionClient
 from geometry_msgs.msg import TwistStamped
+from franka_msgs.action import Move, Grasp
 from threading import Thread
-from rcdt_utilities.gamepad import Gamepad
-from rcdt_utilities.virtual_gamepad import VirtualGamepad
-
-
-@dataclass
-class Vec3:
-    x: float
-    y: float
-    z: float
+from rcdt_utilities.gamepad import Gamepad, GamepadCommand
 
 
 class GamepadNode(Node):
     def __init__(self):
-        self.scale_factor = 0.5
+        self.scaler = 0.5
         super().__init__("rcdt_gamepad")
-        self.declare_parameter("virtual", True)
         self.topic = "/servo_node/delta_twist_cmds"
-        self.frame_id = "fr3_link0"
         self.publisher = self.create_publisher(TwistStamped, self.topic, 10)
-        self.vector = Vec3(0, 0, 0)
+        self.move_client = ActionClient(self, Move, "/fr3_gripper/move")
+        self.grasp_client = ActionClient(self, Grasp, "/fr3_gripper/grasp")
+        self.msg = TwistStamped()
+        self.msg.header.frame_id = "fr3_link0"
+        self.gripper_state = "close"
         self.timer = self.create_timer(1 / 100.0, self.publish)
 
     def publish(self) -> None:
-        msg = TwistStamped()
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.header.frame_id = self.frame_id
-        msg.twist.linear.x = float(self.vector.x)
-        msg.twist.linear.y = float(self.vector.y)
-        msg.twist.linear.z = float(self.vector.z)
-        self.publisher.publish(msg)
+        self.msg.header.stamp = self.get_clock().now().to_msg()
+        self.publisher.publish(self.msg)
 
-    def update_vector(self, x: float, y: float, z: float) -> None:
-        self.vector = Vec3(
-            x * self.scale_factor, y * self.scale_factor, z * self.scale_factor
-        )
+    def update(self, command: GamepadCommand) -> None:
+        for movement in ["linear", "angular"]:
+            command_vector = getattr(command, movement)
+            message_vector = getattr(self.msg.twist, movement)
+            for axis in ["x", "y", "z"]:
+                value = self.scaler * getattr(command_vector, axis)
+                deadzone = 0.1
+                if abs(value) < deadzone:
+                    value = 0.0
+                setattr(message_vector, axis, value)
+        if command.gripper != self.gripper_state:
+            self.update_gripper(command.gripper)
+
+    def update_gripper(self, command: str) -> None:
+        if command == "open":
+            client = self.move_client
+            goal = Move.Goal()
+            goal.width = 0.08
+            goal.speed = 0.03
+        elif command == "close":
+            client = self.grasp_client
+            goal = Grasp.Goal()
+            goal.width = 0.0
+            goal.speed = 0.03
+            goal.epsilon.inner = 0.08
+            goal.epsilon.outer = 0.08
+            goal.force = 100.0
+        else:
+            return
+        client.wait_for_server()
+        result: (
+            Move.Impl.GetResultService.Response | Grasp.Impl.GetResultService.Response
+        ) = client.send_goal(goal)
+        if result.result.success:
+            self.gripper_state = command
+        else:
+            self.get_logger().warn("Gripper move did not succeed.")
 
 
 def main(args: str = None) -> None:
-    """Start the gamepadnode and the (virtual) gamepad in a separate thread."""
     rclpy.init(args=args)
     gamepad_node = GamepadNode()
-    virtual = gamepad_node.get_parameter("virtual").get_parameter_value().bool_value
-    gamepad = VirtualGamepad() if virtual else Gamepad()
-    gamepad.set_callback(gamepad_node.update_vector)
+    gamepad = Gamepad()
+    gamepad.set_callback(gamepad_node.update)
     Thread(target=gamepad.run, daemon=True).start()
 
     rclpy.spin(gamepad_node)
--- rcdt_utilities/gamepad.py
+++ rcdt_utilities/gamepad.py
@@ -2,59 +2,205 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from dataclasses import dataclass
-from inputs import get_gamepad
+import os
+import yaml
+import pygame
 from typing import Callable
+from dataclasses import dataclass
+import dearpygui.dearpygui as dpg
+import dearpygui_grid as dpg_grid
 
 
 @dataclass
-class Axes:
-    x: float
-    y: float
-    rx: float
-    ry: float
+class Vec3:
+    x: float = 0.0
+    y: float = 0.0
+    z: float = 0.0
 
 
-mapping = {
-    "ABS_X": "x",
-    "ABS_RX": "rx",
-    "ABS_Y": "y",
-    "ABS_RY": "ry",
-}
+@dataclass
+class GamepadCommand:
+    linear = Vec3()
+    angular = Vec3()
+    gripper = "closed"
 
 
-def default_callback(x: float, y: float, z: float) -> None:
-    print(x, y, z)
+def default_callback(command: GamepadCommand) -> None:
+    for movement in ["linear", "angular"]:
+        print(f"{movement}: ", end="")
+        vector = getattr(command, movement)
+        for axis in ["x", "y", "z"]:
+            print(f"{axis}: {round(getattr(vector, axis), 1)}, ", end="")
+    print(f"gripper: {command.gripper}", end="")
+    print()
 
 
 class Gamepad:
     def __init__(self):
         self.callback = default_callback
-        self.deadzone = 3000
-        self.axis_max = 32768
+        self.command = GamepadCommand()
+        self.selected_direction = "y"
+        self.initialize_gamepads()
+
+    def set_callback(self, callback: Callable) -> None:
+        self.callback = callback
+
+    def initialize_gamepads(self) -> None:
+        pygame.init()
+        config_path = os.path.join(os.path.dirname(__file__), "gamepad.yaml")
+        with open(config_path, "r") as stream:
+            self.config = yaml.safe_load(stream)
+        self.joysticks = {}
+        for i in range(0, pygame.joystick.get_count()):
+            joystick = pygame.joystick.Joystick(i)
+            if joystick.get_name() in self.config:
+                self.joysticks[i] = joystick
+                joystick.init()
+
+    def run(self) -> None:
+        if not len(self.joysticks):
+            self.run_virtual_gamepad()
+            return
+        clock = pygame.time.Clock()
+        while True:
+            clock.tick(60)
+            for event in pygame.event.get():
+                if "joy" in event.dict:
+                    self.handle_event(event.dict)
+
+    def run_virtual_gamepad(self) -> None:
+        virtual_gamepad = VirtualGamepad()
+        virtual_gamepad.set_callback(self.callback)
+        virtual_gamepad.run()
+
+    def handle_event(self, event: dict) -> None:
+        if "axis" in event:
+            self.handle_axis(event)
+        elif "button" in event:
+            self.handle_button(event)
+        elif "hat" in event:
+            self.handle_hat(event)
+        else:
+            return
+        self.callback(self.command)
+
+    def handle_hat(self, event: dict) -> None:
+        if abs(event["value"][0]):
+            self.selected_direction = "x"
+            self.command.angular.y = 0
+        elif abs(event["value"][1]):
+            self.selected_direction = "y"
+            self.command.angular.x = 0
+
+    def handle_button(self, event: dict) -> None:
+        joystick: pygame.joystick.JoystickType = self.joysticks[event["joy"]]
+        mapping = self.config[joystick.get_name()]
+        buttons = mapping["buttons"]
+        button = event["button"]
+        if button not in buttons:
+            return
+        if buttons[button] == "gripper_open":
+            self.command.gripper = "open"
+        elif buttons[button] == "gripper_close":
+            self.command.gripper = "close"
+
+    def handle_axis(self, event: dict) -> None:
+        joystick: pygame.joystick.JoystickType = self.joysticks[event["joy"]]
+        mapping = self.config[joystick.get_name()]
+        axes = mapping["axes"]
+        axis = event["axis"]
+        if axis not in axes:
+            return
+        movement, direction = axes[axis]["movement"], axes[axis]["direction"]
+        direction = self.selected_direction if direction == "selected" else direction
+        vector = getattr(self.command, movement)
+        value = event["value"]
+        if "flip" in axes[axis] and axes[axis]["flip"] == "true":
+            value *= -1
+        if "sign" in axes[axis]:
+            if axes[axis]["sign"] == "positive":
+                value = (value + 1) / 2
+            elif axes[axis]["sign"] == "negative":
+                value = -(value + 1) / 2
+        setattr(vector, direction, value)
+
+
+class VirtualGamepad:
+    def __init__(self) -> None:
+        self.command = GamepadCommand()
 
     def set_callback(self, callback: Callable) -> None:
         self.callback = callback
 
     def run(self) -> None:
-        last_known_state = Axes(0, 0, 0, 0)
-        while 1:
-            events = get_gamepad()
-            event = events[0]
-            if event.code in mapping:
-                if abs(event.state) < self.deadzone:
-                    setattr(last_known_state, mapping[event.code], 0)
-                    continue
-                setattr(
-                    last_known_state, mapping[event.code], event.state / self.axis_max
-                )
+        dpg.create_context()
+        dpg.create_viewport(title="Virtual Gamepad", width=400, height=400)
 
-            if event.code == "SYN_REPORT":
-                self.callback(
-                    -last_known_state.y,
-                    -max([last_known_state.x, last_known_state.rx], key=abs),
-                    -last_known_state.ry,
-                )
+        with dpg.item_handler_registry(tag="handler"):
+            dpg.add_item_activated_handler(callback=self.button_callback)
+            dpg.add_item_deactivated_handler(callback=self.button_callback)
+
+        self.create_window()
+
+        dpg.setup_dearpygui()
+        dpg.show_viewport()
+        dpg.start_dearpygui()
+        dpg.destroy_context()
+
+    def create_window(self) -> None:
+        window = dpg.add_window(width=-1, height=-1)
+        dpg.set_primary_window(window, True)
+
+        cols = 2
+        rows = 7
+        grid = dpg_grid.Grid(cols, rows, window)
+        grid.offsets = 8, 8, 8, 8
+
+        linear_buttons = ["X+", "Z-", "X-", "Z+", "Y-", "Y+"]
+        other_buttons = ["G-", "G+", "Z-r", "Z+r", "Y-r", "Y+r", "X-r", "X+r"]
+        labels = linear_buttons + other_buttons
+        colors = {
+            "X": (200, 0, 0),
+            "Y": (0, 200, 0),
+            "Z": (0, 0, 200),
+            "G": (200, 200, 200),
+        }
+        row, col = 0, 0
+        for label in labels:
+            uid = dpg.generate_uuid()
+            button = dpg.add_button(parent=window, label=label, tag=uid)
+            dpg.bind_item_handler_registry(uid, "handler")
+            self.set_color(uid, colors[label[0]])
+            grid.push(button, col, row)
+            col = col + 1 if col < cols - 1 else 0
+            row = row + 1 if col == 0 else row
+
+        with dpg.item_handler_registry() as window_hr:
+            dpg.add_item_visible_handler(callback=grid)
+        dpg.bind_item_handler_registry(window, window_hr)
+
+    def set_color(self, tag: int, rgb: tuple) -> None:
+        with dpg.theme() as item_theme, dpg.theme_component(dpg.mvAll):
+            dpg.add_theme_color(dpg.mvThemeCol_Button, rgb)
+        dpg.bind_item_theme(tag, item_theme)
+
+    def button_callback(self, sender: int, app_data: int) -> None:
+        label = dpg.get_item_label(app_data)
+        int_press = 22
+        active = sender == int_press
+        axis = label[0].lower()
+        movement = "angular" if label[-1] == "r" else "linear"
+        value = 1.0 if label[1] == "+" else -1.0
+        value = 0.0 if not active else value
+
+        if axis in ["x", "y", "z"]:
+            vector = getattr(self.command, movement)
+            setattr(vector, axis, value)
+        elif label == "G-":
+            self.command.gripper = "open"
+        elif label == "G+":
+            self.command.gripper = "close"
+        self.callback(self.command)
 
 
 if __name__ == "__main__":
