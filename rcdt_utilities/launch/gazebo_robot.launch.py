91c0c352ed45c71758342bf3a4191dde1a5ad7bb
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2024-11-18 16:05:43 +0000

    Fix topic names and depth camera.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- rcdt_utilities/launch/gazebo_robot.launch.py
+++ rcdt_utilities/launch/gazebo_robot.launch.py
@@ -9,7 +9,11 @@ from launch.actions import IncludeLaunchDescription, OpaqueFunction
 from launch_ros.actions import Node
 from rcdt_utilities.launch_utils import LaunchArgument, get_file_path
 
-world_arg = LaunchArgument("world", "empty.sdf")
+default_world = get_file_path(
+    "rcdt_utilities", ["sdf"], "empty_world_with_camera_plugin.xml.sdf"
+)
+
+world_arg = LaunchArgument("world", default_world)
 
 
 def launch_setup(context: LaunchContext) -> List:
@@ -31,16 +35,18 @@ def launch_setup(context: LaunchContext) -> List:
         name="sync_clock",
         arguments=["/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock"],
     )
-    return [gazebo, spawn_robot, sync_clock]
+
+    return [
+        gazebo,
+        spawn_robot,
+        sync_clock
+    ]
 
 
 def generate_launch_description() -> LaunchDescription:
     return LaunchDescription(
         [
             world_arg.declaration,
-            # gazebo,
-            # spawn_robot,
-            # sync_clock,
             OpaqueFunction(function=launch_setup),
         ]
     )
--- rcdt_utilities/launch/realsense.launch.py
+++ rcdt_utilities/launch/realsense.launch.py
@@ -2,32 +2,20 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-
-import os
-from pathlib import Path
-
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchContext, LaunchDescription
-from launch.actions import (
-    IncludeLaunchDescription,
-    OpaqueFunction,
-    SetEnvironmentVariable,
-)
+from launch.actions import IncludeLaunchDescription, OpaqueFunction
 from launch_ros.actions import Node, SetParameter
 from rcdt_utilities.launch_utils import (
     get_file_path,
     get_robot_description,
 )
 
-pkg_share = get_package_share_directory("rcdt_realsense")
-
 
 def launch_setup(_context: LaunchContext) -> None:
     xacro_path = get_file_path(
         "rcdt_utilities", ["urdf"], "rcdt_realsense_d435.urdf.xacro"
     )
-    xacro_arguments = {"simulation": "true", "parent": "world"}
-    robot_description = get_robot_description(xacro_path, xacro_arguments)
+    robot_description = get_robot_description(xacro_path)
 
     robot_state_publisher = Node(
         package="robot_state_publisher",
@@ -36,26 +24,14 @@ def launch_setup(_context: LaunchContext) -> None:
     )
 
     robot = IncludeLaunchDescription(
-        get_file_path("rcdt_utilities", ["launch"], "gazebo_robot.launch.py"),
-        launch_arguments={
-            "world": "/home/yuripro/husarion_ws/src/husarion_gz_worlds/worlds/husarion_world.sdf"
-        }.items(),
-    )
-
-    ign_resource_path = SetEnvironmentVariable(
-        name="IGN_GAZEBO_RESOURCE_PATH",
-        value=[
-            os.path.join(pkg_share, "worlds"),
-            ":" + str(Path(pkg_share).parent.resolve()),
-        ],
+        get_file_path("rcdt_utilities", ["launch"], "gazebo_robot.launch.py")
     )
 
     rgb_bridge = Node(
         package="ros_gz_image",
         executable="image_bridge",
         arguments=[
-            "/some_name/color/image_raw",
-            # "/camera/camera/color/image_raw"
+            "/camera/color/image_raw",
         ],
         output="screen",
     )
@@ -64,7 +40,7 @@ def launch_setup(_context: LaunchContext) -> None:
         package="ros_gz_bridge",
         executable="parameter_bridge",
         arguments=[
-            "/ns/namespace/depth/image_rect_raw/points@sensor_msgs/msg/PointCloud2[ignition.msgs.PointCloudPacked",
+            "/camera/depth/image_rect_raw/points@sensor_msgs/msg/PointCloud2[ignition.msgs.PointCloudPacked",
         ],
         output="screen",
     )
@@ -72,17 +48,14 @@ def launch_setup(_context: LaunchContext) -> None:
     rviz = IncludeLaunchDescription(
         get_file_path("rcdt_utilities", ["launch"], "rviz.launch.py"),
         launch_arguments={
-            # TODO: placeholder
-            "rviz_frame": "/world",
             "rviz_display_config": get_file_path(
                 "rcdt_utilities", ["rviz"], "realsense.rviz"
-            ),
+            )
         }.items(),
     )
 
     return [
         SetParameter(name="use_sim_time", value=True),
-        ign_resource_path,
         robot_state_publisher,
         robot,
         rgb_bridge,
