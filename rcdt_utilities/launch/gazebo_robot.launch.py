fd84b613b5db15f0ab769a10c1139d48057a3da5
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2024-11-19 11:42:31 +0000

    Fix for use with franka.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- rcdt_utilities/launch/gazebo_robot.launch.py
+++ rcdt_utilities/launch/gazebo_robot.launch.py
@@ -9,17 +9,25 @@ from launch.actions import IncludeLaunchDescription, OpaqueFunction
 from launch_ros.actions import Node
 from rcdt_utilities.launch_utils import LaunchArgument, get_file_path
 
-default_world = get_file_path(
-    "rcdt_utilities", ["sdf"], "empty_world_with_camera_plugin.xml.sdf"
-)
-
-world_arg = LaunchArgument("world", default_world)
+load_gazebo_ui_arg = LaunchArgument("load_gazebo_ui", False, [True, False])
+world_arg = LaunchArgument("world", "")
+use_realsense_arg = LaunchArgument("realsense", False, [True, False])
 
 
 def launch_setup(context: LaunchContext) -> List:
+    load_gazebo_ui = load_gazebo_ui_arg.value(context)
+    world = world_arg.value(context)
+    use_realsense = use_realsense_arg.value(context)
+
+    if world == "":
+        world = get_file_path("rcdt_utilities", ["sdf"], "empty.xml.sdf")
+
+    gz_args = f" -r {world}"
+    if not load_gazebo_ui:
+        gz_args += " -s"
     gazebo = IncludeLaunchDescription(
         get_file_path("ros_gz_sim", ["launch"], "gz_sim.launch.py"),
-        launch_arguments={"gz_args": f" -r {world_arg.value(context)}"}.items(),
+        launch_arguments={"gz_args": gz_args}.items(),
     )
 
     spawn_robot = Node(
@@ -29,24 +37,35 @@ def launch_setup(context: LaunchContext) -> List:
         output="screen",
     )
 
-    sync_clock = Node(
+    bridge_topics = ["/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock"]
+    if use_realsense:
+        bridge_topics.extend(
+            [
+                "/camera/color/image_raw@sensor_msgs/msg/Image@gz.msgs.Image",
+                "/camera/color/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo",
+                "/camera/depth/image_rect_raw/points@sensor_msgs/msg/PointCloud2[ignition.msgs.PointCloudPacked",
+            ]
+        )
+
+    bridge = Node(
         package="ros_gz_bridge",
         executable="parameter_bridge",
-        name="sync_clock",
-        arguments=["/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock"],
+        arguments=bridge_topics,
     )
 
     return [
         gazebo,
         spawn_robot,
-        sync_clock
+        bridge,
     ]
 
 
 def generate_launch_description() -> LaunchDescription:
     return LaunchDescription(
         [
+            load_gazebo_ui_arg.declaration,
             world_arg.declaration,
+            use_realsense_arg.declaration,
             OpaqueFunction(function=launch_setup),
         ]
     )
